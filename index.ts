c4778230529feb4a8876da2b5b33020d3afff6a5
Rajeev Atla <rajeev@rajeevatla.com>
2024-04-02 20:09:15 -0400

    add test file

--- index.ts
+++ index.ts
@@ -1,8 +1,18 @@
+import { which } from 'bun';
 import data from './states.json';
+import {isPointInPoly, whichState, getData, type StateBorders} from './util.ts'
 
-interface StateBorders {
-      state: string;
-      border: Array<Array<decimal>>;
-};
 
-console.log(data)
+let jsonData = data.states;
+let StateBordersData: Array<StateBorders> = [];
+
+jsonData.forEach((element) => StateBordersData.push({state: element.state, border: element.border}));
+
+let testLong: number = -77.036133;
+let testLat: number = 40.51379;
+
+let testPt: Array<number> = [testLong, testLat];
+
+//let penn: StateBorders = StateBordersData.find((state) => state.state == "Pennsylvania");
+
+console.log(whichState(testPt, StateBordersData));
